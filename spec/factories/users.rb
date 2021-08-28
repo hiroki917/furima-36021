64a165d438b58faa72b2ed1ecd7706ac68495002
hiroki917 <kensmith_5276_zemaitis@yahoo.co.jp>
2021-08-29 07:02:24 +0900

    Rubocopの設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {'1abcde'}
-    password_confirmation {password}
-    first_name            {"小柳津"}
-    last_name             {"洋毅"}
-    first_name_kana       {"オヤイヅ"}
-    last_name_kana        {"ヒロキ"}
-    birth_day             {"1986-09-17"}
-    
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { '1abcde' }
+    password_confirmation { password }
+    first_name            { '小柳津' }
+    last_name             { '洋毅' }
+    first_name_kana       { 'オヤイヅ' }
+    last_name_kana        { 'ヒロキ' }
+    birth_day             { '1986-09-17' }
   end
 end
