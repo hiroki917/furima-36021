cfac7836b56f9dffb93147ad696cf95b1ece5b4e
hiroki917 <kensmith_5276_zemaitis@yahoo.co.jp>
2021-08-29 11:32:23 +0900

    テストコード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :nickname,           presence: true
   validates :birth_day,          presence: true
-  # validates :first_name,         presence: true, presence: {message: 'cant be blank'}
+  
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: ' is invalid. Input full-width characters' } do
     validates :last_name
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -125,8 +125,8 @@ RSpec.describe User, type: :model do
       end
 
       it 'passwordが5文字以下では登録できない' do
-        @user.password = '00000'
-        @user.password_confirmation = '00000'
+        @user.password = '11aaa'
+        @user.password_confirmation = '11aaa'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
