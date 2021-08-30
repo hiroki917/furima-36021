fb24796a8743342e10bcb835413f8ed72ef1a61a
hiroki917 <kensmith_5276_zemaitis@yahoo.co.jp>
2021-08-30 23:26:28 +0900

    バリデーションを設定とActiveHashの中身作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,28 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_charge
+  belongs_to :area
+  belongs_to :day
+
+  # validates :area_id, numericality:            { other_than: 1 , message: "can't be blank"}
+  # validates :category_id, numericality:        { other_than: 1 , message: "can't be blank"}
+  # validates :day_id, numericality:             { other_than: 1 , message: "can't be blank"}
+  # validates :status_id, numericality:          { other_than: 1 , message: "can't be blank"}
+  # validates :shipping_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  with_options numericality:    { other_than: 1 , message: "can't be blank"} do
+    validates :area_id
+    validates :category_id
+    validates :day_id
+    validates :status_id
+    validates :shipping_charge_id
+  end  
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9,999,999 }
+  validates :price, numericality: { only_integer: true }
+  validates :name, :explanation, :category_id, :status_id, 
+  :shipping_charge_id, :area_id, :day_id, :price, :image, presence: true
+  belongs_to :user
 end
