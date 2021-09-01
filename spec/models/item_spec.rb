a6cd8230cb1ce24161438baea5bdc0c0c7e3484a
hiroki917 <kensmith_5276_zemaitis@yahoo.co.jp>
2021-09-01 19:26:14 +0900

    修正依頼完了

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -70,6 +70,16 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
+      it '画像が空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'userが紐づいていなければ登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
     end
   end
 end
