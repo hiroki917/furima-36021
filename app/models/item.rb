2d136eef4a9fc4ee90f39ccf00c26a010b1a5a2f
hiroki917 <kensmith_5276_zemaitis@yahoo.co.jp>
2021-09-02 21:35:25 +0900

    条件分岐実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  has_many :purchases
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
